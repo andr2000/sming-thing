48edd28d0e8d10402f2423eac42654c2bbb634bf
Oleksandr Andrushchenko <andr2000@gmail.com>
2019-01-23 11:56:44 +0200

    Use autoconf.h in the application
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- app/application.cpp
+++ app/application.cpp
@@ -1,13 +1,14 @@
 #include <user_config.h>
 #include <SmingCore/SmingCore.h>
 
-//#include "autoconf.h"
+#include "autoconf.h"
 
 void init(void)
 {
 	Serial.begin(SERIAL_BAUD_RATE); // 115200 by default
 	Serial.systemDebugOutput(true); // Debug output to serial
 
-	Serial.printf("Current ROM slot %d", rboot_get_current_rom());
+	Serial.printf("I am " CONFIG_THING_NAME ", revision " VERSION "\n");
+	Serial.printf("Current ROM slot %d\n", rboot_get_current_rom());
 	Serial.println();
 }
